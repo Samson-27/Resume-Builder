ef4cbea2dd585f43d1fdaa6892e4355c8a8680de
Samson-27 <samsonhajong27@gmail.com>
2025-09-03 17:35:14 +0530

    Initial commit0

--- templates/header.php
+++ templates/header.php
@@ -10,17 +10,23 @@
 </head>
 
 <body>
-  <!-- Navbar -->
-  <header>
-    <nav>
-      <h4 class="logo">RESUME BUILDER</h4>
-      <ul>
-        <li><a href="#">HOME</a></li>
-        <li><a href="#">RESUME</a></li>
-        <li><a href="#">LOGIN</a></li>
-      </ul>
-    </nav>
-  </header>
+<!-- Navbar -->
+<nav class="navbar">
+  <div class="logo">Resume Builder</div>
+  <ul class="nav-links">
+    <li><a href="#home">Home</a></li>
+    <li><a href="#features">Features</a></li>
+    <li><a href="#templates">Templates</a></li>
+    <li><a href="#about">About</a></li>
+    <li><a href="#pricing">Pricing</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ul>
+  <div class="nav-btns">
+    <a href="#login" class="btn login">Login</a>
+    <a href="#signup" class="btn signup">Sign Up</a>
+  </div>
+</nav>
+
 </body>
 
 </html>
